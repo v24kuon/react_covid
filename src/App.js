2bfd3c5ef1ad7a5a6aa1415571a7ebd8e971d836
v24kuon <tete@users.noreply.github.com>
2021-12-23 07:44:51 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import countriesJson from "./countries.json";
+import TopPage from "./pages/TopPage";
+import "./App.css";
+
+function App() {
+  const [country, setCountry] = useState("");
+  const [countryData, setCountryData] = useState({
+    date: "",
+    newConfirmed: "",
+    totalConfirmed: "",
+    newRecovered: "",
+    totalRecovered: "",
+  });
+  const getCountryData = () => {
+    fetch(`https://api.covid19api.com/country/${country}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setCountryData({
+          date: data[data.length - 1].Date,
+          newConfirmed: data[data.length - 1].Confirmed - data[data.length - 2].Confirmed,
+          totalConfirmed: data[data.length - 1].Confirmed,
+          newRecovered: data[data.length - 1].Recovered - data[data.length - 2].Recovered,
+          totalRecovered: data[data.length - 1].Recovered,
+        });
+      });
+  };
+  return (
+    <div>
+      <TopPage countriesJson={countriesJson} setCountry={setCountry} getCountryData={getCountryData} countryData={countryData} />
+    </div>
+  );
+}
+export default App;
---
+++ src/pages/TopPage.js
@@ -0,0 +1,17 @@
+import Title from "../components/Title";
+import Selector from "../components/Selector";
+import Results from "../components/Results";
+
+const TopPage = (props) => {
+  return (
+    <div className="top-page-container">
+      <div>
+        <Title />
+        <Selector countriesJson={props.countriesJson} setCountry={props.setCountry} getCountryData={props.getCountryData} />
+        <Results countryData={props.countryData} />
+      </div>
+    </div>
+  );
+};
+
+export default TopPage;
